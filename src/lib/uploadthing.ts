6eef90de7880b13b8addee5a2750de79a5231cf8
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2025-11-08 13:41:52 +0000

    Try fixing this error: `Build Error: Export generateUploadThingHooks doe

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,18 +1,16 @@
-import { generateUploadThingHooks } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 import { auth } from "./firebase";
  
-export const { useUploadThing } = generateUploadThingHooks<OurFileRouter>({
-  hooks: {
-    onUploadBegins: async () => {
-      const user = auth.currentUser;
-      if (user) {
-        const token = await user.getIdToken();
-        return {
-          Authorization: `Bearer ${token}`
-        }
+export const { useUploadThing } = generateReactHelpers<OurFileRouter>({
+  onUploadBegins: async () => {
+    const user = auth.currentUser;
+    if (user) {
+      const token = await user.getIdToken();
+      return {
+        Authorization: `Bearer ${token}`
       }
-      return {};
     }
+    return {};
   }
 });
