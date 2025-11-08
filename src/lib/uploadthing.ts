d5207574f0cb14269093f9c5543323cf3df305a2
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2025-11-08 13:40:55 +0000

    but its still not working

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,5 +1,18 @@
-import { generateReactHelpers } from "@uploadthing/react";
- 
+import { generateUploadThingHooks } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import { auth } from "./firebase";
  
-export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
+export const { useUploadThing } = generateUploadThingHooks<OurFileRouter>({
+  hooks: {
+    onUploadBegins: async () => {
+      const user = auth.currentUser;
+      if (user) {
+        const token = await user.getIdToken();
+        return {
+          Authorization: `Bearer ${token}`
+        }
+      }
+      return {};
+    }
+  }
+});
