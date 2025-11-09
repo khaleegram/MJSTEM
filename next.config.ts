3873f15754847ae8278218a1cdd5a9f6e5f2e538
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2025-11-09 16:55:05 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -32,7 +32,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,
+        protocol: 'https',
         hostname: 'utfs.io',
         port: '',
         pathname: '/**',
