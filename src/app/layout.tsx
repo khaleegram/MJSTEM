e22f7b826beda6b179a006a574618aef39b3a992
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2026-01-11 12:00:50 +0000

    stil bro teh pwa is not instalble on desktops but isntablle on mobile wh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="theme-color" content="#111827" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,400;0,7..72,700;1,7..72,400&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
