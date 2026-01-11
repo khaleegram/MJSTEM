b8c2810f9638103725fc14d1b7cc49d91ce668a5
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2026-01-11 11:09:40 +0000

    no if you can use teh app icon that we use acrros all places currently t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,400;0,7..72,700;1,7..72,400&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📚</text></svg>" />
       </head>
       <body className={cn("font-body antialiased", fontSans.variable)}>
         <ThemeProvider
